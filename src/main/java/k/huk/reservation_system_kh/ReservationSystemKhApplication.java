9d235dda0c0faf6161593877d5ffcc2d2bcd57a5
Kirill Huk <Kirill.Huk@fleetcor.com>
2026-04-14 18:05:15 +0300

    init

---
+++ src/main/java/k/huk/reservation_system_kh/ReservationSystemKhApplication.java
@@ -0,0 +1,13 @@
+package k.huk.reservation_system_kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemKhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemKhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/k/huk/reservation_system_kh/ReservationSystemKhApplicationTests.java
@@ -0,0 +1,13 @@
+package k.huk.reservation_system_kh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemKhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
